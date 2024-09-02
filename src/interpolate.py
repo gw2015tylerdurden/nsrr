353207d0763b1455291aec4ca8f336931957854c
yusuke <yusuke.sakai@feynman.sv.yc.tcu.ac.jp>
2024-09-02 18:09:07 +0900

    fix interp

--- src/interpolate.py
+++ src/interpolate.py
@@ -6,22 +6,24 @@ import numpy as np
 class Interpolator(ABC):
 
     @abstractmethod
-    def interpolate(self, y, x, x_new, kind):
+    def interpolate(self, y, x, x_new):
         pass
 
     @classmethod
-    def get_instance(cls, method):
-        if method in ['cubic', 'linear', 'akima']:
-            return Interpolator1d()
+    def get_instance(cls, kind):
+        if kind in ['cubic', 'linear', 'akima']:
+            return Interpolator1d(kind)
         else:
-            raise ValueError(f"Unknown interpolation method: {method}")
+            raise ValueError(f"Unknown interpolation method: {kind}")
 
 
 class Interpolator1d(Interpolator):
+    def __init__(self, kind):
+        self.kind = kind
 
-    def interpolate(self, y, x, x_new, kind='linear'):
-        if kind == 'akima':
+    def interpolate(self, y, x, x_new):
+        if self.kind == 'akima':
             f = Akima1DInterpolator(x, y)
         else:
-            f = interp1d(x, y, kind=kind)
+            f = interp1d(x, y, kind=self.kind)
         return f(x_new)
